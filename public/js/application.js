0de7cd5211dfae049a57f18d56f578027df2940e
Sarah Taurchini <sarah.taurchini@gmail.com>
2015-03-18 15:05:19 -0400

    add map initialization to js file

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  L.mapbox.accessToken = 'pk.eyJ1IjoidGhlaHVtYW50YXVyY2giLCJhIjoiLUJ5Nkd6NCJ9.dCa7lSVwCoV9n9mDoWgwhg'
+  L.mapbox.map('map', 'thehumantaurch.lg48lh4f');
+  // L.marker([50.5, 30.5]).addTo(map);
 });
