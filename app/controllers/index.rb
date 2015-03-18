b84639fc55e24abe82443cf020eeba3c379baa89
Sarah Taurchini <sarah.taurchini@gmail.com>
2015-03-18 12:33:47 -0400

    import theaters/shows to database on page load

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,29 @@
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
+before '/' do
+  doc = Nokogiri::HTML(open("http://theatrewashington.org/find-a-show?page=full"))
+
+  shows = []
+  theaters = []
+
+  doc.css('.tw-show-list-title').each do |link|
+    shows << Performance.find_or_create_by(show_title: link.content)
+  end
+
+  doc.css('.tw-theater-list-title').each do |link|
+    theaters << Theater.find_or_create_by(name: link.content)
+  end
+
+  test = shows.zip(theaters)
+
+  test.each do |combo|
+    combo[0].update(theater_id: combo[1].id)
+  end
+
+end
+
 get '/' do
   erb :index
 end
