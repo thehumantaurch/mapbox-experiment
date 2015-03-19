ce311d059545141679a47b00cf17b7b0119ccb52
Sarah Taurchini <sarah.taurchini@gmail.com>
2015-03-19 17:41:15 -0400

    add markers for each show location

--- public/js/application.js
+++ public/js/application.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
-  L.mapbox.accessToken = 'pk.eyJ1IjoidGhlaHVtYW50YXVyY2giLCJhIjoiLUJ5Nkd6NCJ9.dCa7lSVwCoV9n9mDoWgwhg'
-  L.mapbox.map('map', 'thehumantaurch.lg48lh4f');
-  // L.marker([50.5, 30.5]).addTo(map);
+  L.mapbox.accessToken = 'pk.eyJ1IjoidGhlaHVtYW50YXVyY2giLCJhIjoiLUJ5Nkd6NCJ9.dCa7lSVwCoV9n9mDoWgwhg';
+
+  var map = L.mapbox.map('map', 'thehumantaurch.lg48lh4f');
+
+  for (i = 0; i < gon.performances.length; i++) {
+    marker = new L.marker([
+      gon.performances[i].latitude,
+      gon.performances[i].longitude])
+    .bindPopup(gon.performances[i].latitude, gon.performances[i].longitude)
+    .addTo(map);
+  }
+
 });
+
