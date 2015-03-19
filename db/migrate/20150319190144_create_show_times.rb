25ea23df0ee9a4c112c3d08a4d6b9cd22fa766e8
Sarah Taurchini <sarah.taurchini@gmail.com>
2015-03-19 19:04:15 -0400

    update field for date and time of show

--- db/migrate/20150319190144_create_show_times.rb
+++ db/migrate/20150319190144_create_show_times.rb
@@ -2,5 +2,7 @@ class CreateShowTimes < ActiveRecord::Migration
   def change
     create_table :show_times do |t|
       t.references :performance
+      t.datetime :day_and_time
+    end
   end
 end
