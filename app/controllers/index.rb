7a288d546e6f33bd19e32241f4f4109c2f060dfd
Sarah Taurchini <sarah.taurchini@gmail.com>
2015-03-19 16:37:58 -0400

    add gon performances to access with JS

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,29 +1,34 @@
 require 'open-uri'
 require 'nokogiri'
 require 'pry'
+require 'geocoder'
+require 'gon-sinatra'
 
-before '/' do
-  doc = Nokogiri::HTML(open("http://theatrewashington.org/find-a-show?page=full"))
+Sinatra::register Gon::Sinatra
+# before '/' do
+#   doc = Nokogiri::HTML(open("http://theatrewashington.org/find-a-show?page=full"))
 
-  shows = []
-  theaters = []
+#   shows = []
+#   doc.css('.readon.blue').each do |link|
+#     shows << link.attributes["href"].value
+#   end
 
-  doc.css('.tw-show-list-title').each do |link|
-    shows << Performance.find_or_create_by(show_title: link.content)
-  end
+#   shows.each do |link|
+#     doc = Nokogiri::HTML(open("http://theatrewashington.org/#{link}"))
 
-  doc.css('.tw-theater-list-title').each do |link|
-    theaters << Theater.find_or_create_by(name: link.content)
-  end
+#     @theater = Theater.find_or_create_by(name: doc.css('#rt-main > div > div.rt-grid-12 > div > div > div > div.rt-module-inner > div > div > div.rt-article > div.module-inner > div > div.production-col1 > p.last > a').children.text)
 
-  test = shows.zip(theaters)
+#     @performance = Performance.find_or_create_by(
+#       show_title: doc.css('#rt-main > div > div.rt-grid-12 > div > div > div > div.rt-module-inner > div > div > div.rt-article > div.rt-headline').children.text.strip,
+#       theater_id: @theater.id,
+#       address: doc.css('#rt-main > div > div.rt-grid-12 > div > div > div > div.rt-module-inner > div > div > div.rt-article > div.module-inner > div > div.production-col2 > div.rt-article > div.module-inner > div > div.address').children.text.strip
+#       )
+#   end
 
-  test.each do |combo|
-    combo[0].update(theater_id: combo[1].id)
-  end
-
-end
+# end
 
 get '/' do
+  @performances = Performance.all
+  gon.performances = @performances
   erb :index
 end
