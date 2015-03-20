932893c78baaa9eb62ddb0caf7b8a17e9fdd75fb
Sarah Taurchini <sarah.taurchini@gmail.com>
2015-03-20 11:02:33 -0400

    edit to accomodate end and start dates as date types

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,6 +3,7 @@ require 'nokogiri'
 require 'pry'
 require 'geocoder'
 require 'gon-sinatra'
+require 'date'
 
 Sinatra::register Gon::Sinatra
 enable :sessions
@@ -21,17 +22,18 @@ before '/' do
     shows.each do |link|
       doc = Nokogiri::HTML(open("http://theatrewashington.org/#{link}"))
 
-      @theater = Theater.find_or_create_by(name: doc.css('#rt-main > div > div.rt-grid-12 > div > div > div > div.rt-module-inner > div > div > div.rt-article > div.module-inner > div > div.production-col1 > p.last > a').children.text)
-
       price_range = doc.css("#performances-description-specifics").children.children.children.text.split("\t\t\t")[-1].strip.split("-")
+
+      start_date = Date.strptime(doc.css("#performances-curtain-times > caption > span > span:nth-child(1)").children.text, "%m/%d/%Y")
+      end_date = Date.strptime(doc.css("#performances-curtain-times > caption > span > span:nth-child(2)").children.text, '%m/%d/%Y')
 
       @performance = Performance.find_or_create_by(
         show_title: doc.css('#rt-main > div > div.rt-grid-12 > div > div > div > div.rt-module-inner > div > div > div.rt-article > div.rt-headline').children.text.strip,
-        theater_id: @theater.id,
+        theater_name: doc.css('#rt-main > div > div.rt-grid-12 > div > div > div > div.rt-module-inner > div > div > div.rt-article > div.module-inner > div > div.production-col1 > p.last > a').children.text,
         address: doc.css('#rt-main > div > div.rt-grid-12 > div > div > div > div.rt-module-inner > div > div > div.rt-article > div.module-inner > div > div.production-col2 > div.rt-article > div.module-inner > div > div.address').children.text.strip,
         buy_tickets: doc.css("#rt-main > div > div.rt-grid-12 > div > div > div > div.rt-module-inner > div > div > div.rt-article > div.module-inner > div > div.production-col1 > a")[0].attributes["href"].value,
-        start_date: doc.css("#performances-curtain-times > caption > span > span:nth-child(1)").children.text,
-        end_date: doc.css("#performances-curtain-times > caption > span > span:nth-child(2)").children.text,
+        start_date: start_date,
+        end_date: end_date,
         genre: doc.css("#performances-description-specifics").children.children.children.text.split("\t\t\t")[1].strip,
         price_low: price_range[0].strip.delete("$").to_i,
         price_high: price_range[1].strip.delete("$").to_i
