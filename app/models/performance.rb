b094ede94afc0dbf3ff0417ca77e40bbdbb2af36
Sarah Taurchini <sarah.taurchini@gmail.com>
2015-04-05 23:46:50 -0400

    add Geocoder configuration timeout

--- app/models/performance.rb
+++ app/models/performance.rb
@@ -3,6 +3,8 @@ require 'geocoder'
 class Performance < ActiveRecord::Base
 
   extend Geocoder::Model::ActiveRecord
+  Geocoder::Configuration.timeout = 30
+  binding.pry
 
   geocoded_by :address
   after_validation :geocode
